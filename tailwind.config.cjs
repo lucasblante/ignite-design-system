9dd053a32ebb24093e93536a922a4f98c93fa5f6
Lucas Blante <lucasblante@hotmail.com>
2022-10-11 20:22:47 -0300

    Configuracoes iniciais

---
+++ tailwind.config.cjs
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    // Path to content files
+    './src/**/*.tsx' 
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
